41db7d3b017eb89f608299b6cd7dde6962612766
Nicolas Martyanoff <khaelin@gmail.com>
2019-06-30 14:08:41 +0200

    add code documentation

--- src/icu.erl
+++ src/icu.erl
@@ -4,3 +4,8 @@
 -export_type([ustring/0]).
 
 -type ustring() :: binary().
+%% An UTF-16 binary string.
+%%
+%% We use the term `ustring' to match the C type `UChar' used by ICU, making
+%% it obvious that this is the type used by all ICU functions. In the
+%% documentation, we also use the term "ICU string".
--- src/icu_string.erl
+++ src/icu_string.erl
@@ -5,14 +5,22 @@
          transform_utf8/2, transform/2,
          to_lower/1, to_lower/2, to_upper/1, to_upper/2]).
 
+%% @doc Convert an UTF-8 encoded binary string to an ICU string.
 -spec from_utf8(binary()) -> icu:ustring().
 from_utf8(BinaryString) ->
   icu_nif:str_from_utf8(BinaryString).
 
+%% @doc Convert an ICU string to an UTF-8 encoded binary string.
 -spec to_utf8(icu:ustring()) -> binary().
 to_utf8(UString) ->
   icu_nif:str_to_utf8(UString).
 
+%% @doc Apply a list of transformation functions to an UTF-8 binary string.
+%%
+%% The function simply wraps `transform/2', adding the necessary input and
+%% output format conversions.
+%%
+%% @see transform/2
 -spec transform_utf8(binary(), [Fun]) -> binary() when
     Fun :: fun((icu:ustring()) -> icu:ustring()).
 transform_utf8(BinaryString, Funs) ->
@@ -20,6 +28,10 @@ transform_utf8(BinaryString, Funs) ->
   UString2 = transform(UString, Funs),
   to_utf8(UString2).
 
+%% @doc Apply a list of transformation functions to an ICU string.
+%%
+%% Each transformation is applied to the output of the previous one. The
+%% function returns the output of the last transformation.
 -spec transform(icu:ustring(), [Fun]) -> icu:ustring() when
     Fun :: fun((icu:ustring()) -> icu:ustring()).
 transform(UString, []) ->
@@ -27,18 +39,22 @@ transform(UString, []) ->
 transform(UString, [Fun | Rest]) ->
   transform(Fun(UString), Rest).
 
+%% @doc Convert an ICU string to lower case using the default locale.
 -spec to_lower(icu:ustring()) -> icu:ustring().
 to_lower(UString) ->
   icu_nif:str_to_lower(UString).
 
+%% @doc Convert an ICU string to lower case.
 -spec to_lower(icu:ustring(), string()) -> icu:ustring().
 to_lower(UString, Locale) ->
   icu_nif:str_to_lower(UString, Locale).
 
+%% @doc Convert an ICU string to upper case using the default locale.
 -spec to_upper(icu:ustring()) -> icu:ustring().
 to_upper(UString) ->
   icu_nif:str_to_upper(UString).
 
+%% @doc Convert an ICU string to upper case.
 -spec to_upper(icu:ustring(), string()) -> icu:ustring().
 to_upper(UString, Locale) ->
   icu_nif:str_to_upper(UString, Locale).
